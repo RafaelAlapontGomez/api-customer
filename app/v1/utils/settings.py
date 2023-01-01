be73aa12c0a7b7da0eb664dfe995f1d028e81497
ralapont <rafael.alapont.gomez@gmail.com>
2023-01-01 15:34:20 +0100

    containers docker

--- app/v1/utils/settings.py
+++ app/v1/utils/settings.py
@@ -6,9 +6,9 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Settings(BaseSettings):
-    _db_name: str = os.getenv('DB_NAME')
+    _db_name: str = os.getenv('DB_DATABASE')
     db_user: str = os.getenv('DB_USER')
-    db_pass: str = os.getenv('DB_PASS')
+    db_pass: str = os.getenv('DB_PASSWORD')
     db_host: str = os.getenv('DB_HOST')
     db_port: int = os.getenv('DB_PORT')
 
