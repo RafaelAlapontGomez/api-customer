4bea76880465c1064a2349299cb4579f2b69557c
ralapont <rafael.alapont.gomez@gmail.com>
2023-01-06 22:55:10 +0100

    varios profiles

--- app/v1/utils/settings.py
+++ app/v1/utils/settings.py
@@ -5,6 +5,19 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+ENV = os.getenv('ENV')
+print("ENV == {0}", ENV)
+
+if ENV == 'local':
+    load_dotenv('.env.local')
+    print("Carga .env.local")
+elif ENV == 'docker':
+    load_dotenv('.env.docker')
+    print("Carga .env.docker")
+else:
+    load_dotenv('.env')
+    print("Carga .env")
+
 class Settings(BaseSettings):
     _db_name: str = os.getenv('DB_DATABASE')
     db_user: str = os.getenv('DB_USER')
